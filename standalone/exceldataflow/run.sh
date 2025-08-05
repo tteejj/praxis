d88f5686f7264e517e31e7b5000ab2501c3949a7
tteejj <jhn.heninger#gmail.com>
2025-08-05 11:06:54 -0600

    more standalone

--- standalone/exceldataflow/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-pwsh -NoProfile -File ExcelMappingTool.ps1
--- standalone/exceldataflow/test-simple-dialog.ps1
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env pwsh
-
-# Test the new simplified dialog
-. "$PSScriptRoot/Screens/SimpleStartupDialog.ps1"
-
-Write-Host "Testing simplified dialog..." -ForegroundColor Green
-Test-SimpleStartupDialog
